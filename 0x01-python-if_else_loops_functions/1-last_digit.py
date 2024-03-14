17b79b0d4e0186bf8d740d96eb7530a273fe0d50
asamoah953 <asamoahclinton953@gmail.com>
2024-03-13 17:57:27 -0700

    elif digit == 0:

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,7 @@ if (number < 0):
      print("Last digit of {} is {} and is ".format(number, digit), end="")
 if digit > 5:
     print("greater than 5")
-if(number < 6 and number !=0):
+elif digit == 0:
+    print("0")
+else:
     print("less than 6 and not 0")
