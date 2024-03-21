5b48f979554fa7d280e76d05708ebe9eec377294
asamoah953 <asamoahclinton953@gmail.com>
2024-03-20 17:06:35 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    def square_matrix_simple(matrix=[]):
+        new_matrix = [[num**2 for num in row] for row in matrix]
+        return new_matrix
