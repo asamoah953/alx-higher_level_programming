8667a2a071b824e025141c11c708775e27492b27
asamoah953 <asamoahclinton953@gmail.com>
2024-04-16 10:52:37 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===================================
+This module is a with class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
