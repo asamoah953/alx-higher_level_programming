895ff6f141f58ccf85c0205b2f9333a2d5e91775
asamoah953 <asamoahclinton953@gmail.com>
2024-04-16 10:29:59 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+=============================
+This module is  with the method lookup
+=============================
+"""
+
+
+def lookup(obj):
+    """This function returns the attributes for an object"""
+
+    return dir(obj)
