1740fe5794610c2aaf1f146654309f4be1da5dd6
asamoah953 <asamoahclinton953@gmail.com>
2024-07-31 01:00:00 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,11 @@
-import sys
+#!/usr/bin/node
 
-def read_file(file_path):
-    try:
-        # Open the file with UTF-8 encoding
-        with open(file_path, 'r', encoding='utf-8') as file:
-            # Read the content of the file
-            content = file.read()
-            # Print the content
-            print(content)
-    except Exception as e:
-        # Print the error object if an exception occurs
-        print(f"Error: {e}")
+const fs = require('fs');
 
-if __name__ == "__main__":
-    # Check if a file path argument is provided
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <file_path>")
-    else:
-        # Get the file path from the command-line arguments
-        file_path = sys.argv[1]
-        # Call the read_file function
-        read_file(file_path)
-
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
