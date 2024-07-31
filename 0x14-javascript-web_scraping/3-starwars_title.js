9d9986ad451fd63670abe4473e8337142becfd3a
asamoah953 <asamoahclinton953@gmail.com>
2024-07-31 01:11:49 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
-const endPoint = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request({ url: endPoint, methods: 'GET' }, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(body && JSON.parse(body).title);
-  }
-})
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
